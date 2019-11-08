cd50b980fd247c2d7dbd3826ff95407cd26dfe90
Varinder <varinderdhillon123>
2019-11-08 02:05:40 -0500

    Error Method

---
+++ C0765501_MidTerm_MAD3115F2019/Error/CustomerError.swift
@@ -0,0 +1,13 @@
+//
+//  CustomerError.swift
+//  C0765501_MidTerm_MAD3115F2019
+//
+//  Created by Simranjit Singh Johal on 2019-11-08.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+enum CustomerError : Error
+{
+    case invalidEmail, invalidMobile
+}
